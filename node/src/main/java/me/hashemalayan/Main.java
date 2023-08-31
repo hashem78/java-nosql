800022076529298cd4b1c6234a74b4b2526a344c
Labiba-AI <sourcecontrol@labiba.ai>
2023-08-31 10:33:09 +0300

    Run configs

--- node/src/main/java/me/hashemalayan/Main.java
+++ node/src/main/java/me/hashemalayan/Main.java
@@ -5,10 +5,6 @@ import com.google.inject.Guice;
 public class Main {
     public static void main(String[] args) {
 
-        if (args.length != 1) {
-            System.err.println("ERR: You have to supply a port for the gRPC server");
-            System.exit(1);
-        }
         var injector = Guice.createInjector(
                 new EventLoopModule(),
                 new NodeModule()
