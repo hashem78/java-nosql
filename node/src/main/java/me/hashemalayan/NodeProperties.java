d09ffb37e5a1b664425d171b263e8aef0fb3bbee
hashem78 <hashem.olayano@gmail.com>
2023-10-03 08:50:47 +0300

    Remove Load in NodeProperties

--- node/src/main/java/me/hashemalayan/NodeProperties.java
+++ node/src/main/java/me/hashemalayan/NodeProperties.java
@@ -11,7 +11,6 @@ public class NodeProperties {
     private final int signalingPort = Integer.parseInt(System.getenv("SIGNALING_PORT"));
     private final int port = Integer.parseInt(System.getenv("PORT"));
     private final String name = System.getenv("NAME");
-    private final int load = Integer.parseInt(System.getenv("LOAD"));
     private final boolean useSsl = Boolean.parseBoolean(System.getenv("USE_SSL"));
     private final Path sslCertificatePath = Paths.get(System.getenv("SSL_CERT_PATH"));
     private final Path privateKeyPath = Paths.get(System.getenv("PVT_KEY_PATH"));
